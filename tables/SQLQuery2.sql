f99a035d808e10ae46b9936cf0bd1393ba1bfe97
Leslie Joseph Clarkson <53843306+LeslieJosephClarkson@users.noreply.github.com>
2022-01-31 20:40:54 -0800

    Tables for satellite data

--- tables/SQLQuery2.sql
+++ tables/SQLQuery2.sql
@@ -1,90 +1,90 @@
---CREATE TABLE AO85
---(NumberID int,
---Field varchar(50),
---Bits int,
---Unit varchar(50),
---Conversion int,
---Module varchar(50),
---Module_Num int,
---Module_Line int,
---Line_Type int,
---Short_Name varchar(50),
---) 
+CREATE TABLE AO85
+(NumberID int,
+Field varchar(50),
+Bits int,
+Unit varchar(50),
+Conversion int,
+Module varchar(50),
+Module_Num int,
+Module_Line int,
+Line_Type int,
+Short_Name varchar(50),
+) 
 
---CREATE TABLE AO91
---(NumberID int,
---Field varchar(50),
---Bits int,
---Unit varchar(50),
---Conversion int,
---Module varchar(50),
---Module_Num int,
---Module_Line int,
---Line_Type int,
---Short_Name varchar(50),
---) 
+CREATE TABLE AO91
+(NumberID int,
+Field varchar(50),
+Bits int,
+Unit varchar(50),
+Conversion int,
+Module varchar(50),
+Module_Num int,
+Module_Line int,
+Line_Type int,
+Short_Name varchar(50),
+) 
 
---CREATE TABLE AO95
---(NumberID int,
---Field varchar(50),
---Bits int,
---Unit varchar(50),
---Conversion int,
---Module varchar(50),
---Module_Num int,
---Module_Line int,
---Line_Type int,
---Short_Name varchar(50),
---) 
+CREATE TABLE AO95
+(NumberID int,
+Field varchar(50),
+Bits int,
+Unit varchar(50),
+Conversion int,
+Module varchar(50),
+Module_Num int,
+Module_Line int,
+Line_Type int,
+Short_Name varchar(50),
+) 
 
---CREATE TABLE AO92
---(NumberID int,
---Field varchar(50),
---Bits int,
---Unit varchar(50),
---Conversion int,
---Module varchar(50),
---Module_Num int,
---Module_Line int,
---Line_Type int,
---Short_Name varchar(50),
---) 
+CREATE TABLE AO92
+(NumberID int,
+Field varchar(50),
+Bits int,
+Unit varchar(50),
+Conversion int,
+Module varchar(50),
+Module_Num int,
+Module_Line int,
+Line_Type int,
+Short_Name varchar(50),
+) 
 
---CREATE TABLE Fox1E
---(NumberID int,
---Field varchar(50),
---Bits int,
---Unit varchar(50),
---Conversion int,
---Module varchar(50),
---Module_Num int,
---Module_Line int,
---Line_Type int,
---Short_Name varchar(50),
---) 
+CREATE TABLE Fox1E
+(NumberID int,
+Field varchar(50),
+Bits int,
+Unit varchar(50),
+Conversion int,
+Module varchar(50),
+Module_Num int,
+Module_Line int,
+Line_Type int,
+Short_Name varchar(50),
+) 
 
---CREATE TABLE HO107
---(NumberID int,
---Field varchar(50),
---Bits int,
---Unit varchar(50),
---Conversion int,
---Module varchar(50),
---Module_Num int,
---Module_Line int,
---Line_Type int,
---Short_Name varchar(50),
---) 
+CREATE TABLE HO107
+(NumberID int,
+Field varchar(50),
+Bits int,
+Unit varchar(50),
+Conversion int,
+Module varchar(50),
+Module_Num int,
+Module_Line int,
+Line_Type int,
+Short_Name varchar(50),
+) 
 
---CREATE TABLE GolfT
---(NumberID int,
---Field varchar(50),
---Bits int,
---Unit varchar(50),
---Conversion int,
---Module varchar(50),
---Module_Num int,
---Module_Line int,
---Line_Type int,
---Short_Name varchar(50),
---)
+CREATE TABLE GolfT
+(NumberID int,
+Field varchar(50),
+Bits int,
+Unit varchar(50),
+Conversion int,
+Module varchar(50),
+Module_Num int,
+Module_Line int,
+Line_Type int,
+Short_Name varchar(50),
+)
