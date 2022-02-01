542cbecccbfff97ce783c5dd1cecf843853600ad
Leslie Joseph Clarkson <53843306+LeslieJosephClarkson@users.noreply.github.com>
2022-02-01 15:35:46 -0800

    Tables updated for satellite data

--- tables/SQLQuery2.sql
+++ tables/SQLQuery2.sql
@@ -3,85 +3,111 @@ CREATE TABLE AO85
 timeofenter int
 )
 
-create table ao85max(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
+create table AO85max
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
-create table ao85min(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
+create table AO85min
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
-create table ao85rtt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
-
+create table AO85rtt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
 CREATE TABLE AO91
 (NumberID int,
-Field varchar(50),
-Bits int,
-Unit varchar(50),
-Conversion int,
-Module varchar(50),
-Module_Num int,
-Module_Line int,
-Line_Type int,
-Short_Name varchar(50),
-) 
+timeofenter int
+)
+
+create table AO91max
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
+
+create table AO91min
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
+
+create table AO91rtt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
 CREATE TABLE AO95
 (NumberID int,
 timeofenter int
 )
 
-create table ao95max(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
+create table AO95max
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
-create table ao95min(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
+create table AO95min
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
-create table ao95rtt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
-
+create table AO95rtt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
 CREATE TABLE AO92
 (NumberID int,
-Field varchar(50),
-Bits int,
-Unit varchar(50),
-Conversion int,
-Module varchar(50),
-Module_Num int,
-Module_Line int,
-Line_Type int,
-Short_Name varchar(50),
-) 
+timeofenter int
+)
+
+create table AO92max
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
+
+create table AO92min
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
+
+create table AO92rtt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
 CREATE TABLE Fox1E
 (NumberID int,
-Field varchar(50),
-Bits int,
-Unit varchar(50),
-Conversion int,
-Module varchar(50),
-Module_Num int,
-Module_Line int,
-Line_Type int,
-Short_Name varchar(50),
-) 
+timeofenter int
+)
+
+create table Fox1Emax
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
+
+create table Fox1Emin
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
+
+create table Fox1Ertt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
 CREATE TABLE HO107
 (NumberID int,
 timeofenter int
 )
 
-create table HO107max(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
+create table HO107max
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
-create table HO107min(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
+create table HO107min
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
-create table HO107rtt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
-
+create table HO107rtt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
 
 CREATE TABLE GolfT
 (NumberID int,
-Field varchar(50),
-Bits int,
-Unit varchar(50),
-Conversion int,
-Module varchar(50),
-Module_Num int,
-Module_Line int,
-Line_Type int,
-Short_Name varchar(50),
+timeofenter int
 )
+
+create table GolfTmax
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
+
+create table GolfTmin
+(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
+
+create table GolfTrtt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50)
+)
+
+
