2f1a17f74fdd4a727c989b1a065ecee76fcf78dc
Aaron Jung <71410496+aaronscout622@users.noreply.github.com>
2022-02-01 13:27:16 -0800

    Update SQLQuery2.sql
    max and min and rtt table added

--- tables/SQLQuery2.sql
+++ tables/SQLQuery2.sql
@@ -1,15 +1,14 @@
 CREATE TABLE AO85
 (NumberID int,
-Field varchar(50),
-Bits int,
-Unit varchar(50),
-Conversion int,
-Module varchar(50),
-Module_Num int,
-Module_Line int,
-Line_Type int,
-Short_Name varchar(50),
-) 
+timeofenter int
+)
+
+create table ao85max(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
+
+create table ao85min(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
+
+create table ao85rtt(NumberID int,Field varchar(50),Bits int,Unit varchar(50),Conversion int,Module varchar(50),Module_Num int,Module_Line int,Line_Type int,short_name varchar(50),details varchar(50))
+
 
 CREATE TABLE AO91
 (NumberID int,
