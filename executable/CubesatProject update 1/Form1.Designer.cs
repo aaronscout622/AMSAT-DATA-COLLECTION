18d38a99d70e01eb8f6c272c31d8578702854c53
Trystan Vasquez <98122202+tophthebandit@users.noreply.github.com>
2022-03-01 13:27:36 -0800

    Add files via upload

---
+++ executable/CubesatProject update 1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CubesatProject
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.bypass = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(384, 139);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(384, 39);
+            this.emailTextBox.TabIndex = 0;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(384, 83);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(82, 32);
+            this.emailLabel.TabIndex = 1;
+            this.emailLabel.Text = "E-Mail";
+            this.emailLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(384, 216);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(111, 32);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password";
+            this.passwordLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(384, 272);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(384, 39);
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // bypass
+            // 
+            this.bypass.Location = new System.Drawing.Point(12, 12);
+            this.bypass.Name = "bypass";
+            this.bypass.Size = new System.Drawing.Size(150, 79);
+            this.bypass.TabIndex = 4;
+            this.bypass.Text = "Temporary Bypass";
+            this.bypass.UseVisualStyleBackColor = true;
+            this.bypass.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(384, 361);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(150, 46);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 561);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.bypass);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.emailTextBox);
+            this.Name = "LoginPage";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox emailTextBox;
+        private Label emailLabel;
+        private Label passwordLabel;
+        private TextBox passwordTextBox;
+        private Button bypass;
+        private Button loginButton;
+    }
+}
---
+++ executable/CubesatProject update 1/UserControl4.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CubesatProject
+{
+    partial class profilePage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.profileBackground = new System.Windows.Forms.Panel();
+            this.profileLabel = new System.Windows.Forms.Label();
+            this.profileBackground.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // profileBackground
+            // 
+            this.profileBackground.Controls.Add(this.profileLabel);
+            this.profileBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.profileBackground.Location = new System.Drawing.Point(0, 0);
+            this.profileBackground.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.profileBackground.Name = "profileBackground";
+            this.profileBackground.Size = new System.Drawing.Size(1254, 645);
+            this.profileBackground.TabIndex = 0;
+            this.profileBackground.Paint += new System.Windows.Forms.PaintEventHandler(this.profileBackground_Paint);
+            // 
+            // profileLabel
+            // 
+            this.profileLabel.AutoSize = true;
+            this.profileLabel.Font = new System.Drawing.Font("Segoe UI", 28.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.profileLabel.Location = new System.Drawing.Point(529, 14);
+            this.profileLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.profileLabel.Name = "profileLabel";
+            this.profileLabel.Size = new System.Drawing.Size(128, 51);
+            this.profileLabel.TabIndex = 0;
+            this.profileLabel.Text = "Profile";
+            // 
+            // profilePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.profileBackground);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.Name = "profilePage";
+            this.Size = new System.Drawing.Size(1254, 645);
+            this.profileBackground.ResumeLayout(false);
+            this.profileBackground.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        public Panel profileBackground;
+        public Label profileLabel;
+    }
+}
