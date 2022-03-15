9682bf03f407288447ac33f13682dd28ca4b9c3e
Reginald Winans <91345094+Rwinans15@users.noreply.github.com>
2022-03-15 02:10:26 -0700

    Add files via upload

---
+++ executable/CubesatProject update 2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CubesatProject
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.bypass = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.forgotPass = new System.Windows.Forms.Label();
+            this.wrongLogin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(384, 139);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(384, 39);
+            this.emailTextBox.TabIndex = 0;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(384, 83);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(82, 32);
+            this.emailLabel.TabIndex = 1;
+            this.emailLabel.Text = "E-Mail";
+            this.emailLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(384, 216);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(111, 32);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password";
+            this.passwordLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(384, 272);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(384, 39);
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // bypass
+            // 
+            this.bypass.Location = new System.Drawing.Point(12, 12);
+            this.bypass.Name = "bypass";
+            this.bypass.Size = new System.Drawing.Size(150, 79);
+            this.bypass.TabIndex = 4;
+            this.bypass.Text = "Temporary Bypass";
+            this.bypass.UseVisualStyleBackColor = true;
+            this.bypass.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(384, 361);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(150, 46);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(618, 361);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(150, 46);
+            this.registerButton.TabIndex = 6;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // forgotPass
+            // 
+            this.forgotPass.AutoSize = true;
+            this.forgotPass.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.forgotPass.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.forgotPass.Location = new System.Drawing.Point(379, 445);
+            this.forgotPass.Name = "forgotPass";
+            this.forgotPass.Size = new System.Drawing.Size(199, 32);
+            this.forgotPass.TabIndex = 7;
+            this.forgotPass.Text = "Forgot Password?";
+            // 
+            // wrongLogin
+            // 
+            this.wrongLogin.AutoSize = true;
+            this.wrongLogin.ForeColor = System.Drawing.Color.IndianRed;
+            this.wrongLogin.Location = new System.Drawing.Point(384, 314);
+            this.wrongLogin.Name = "wrongLogin";
+            this.wrongLogin.Size = new System.Drawing.Size(290, 32);
+            this.wrongLogin.TabIndex = 8;
+            this.wrongLogin.Text = "*Incorrect email/password";
+            this.wrongLogin.Visible = false;
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 561);
+            this.Controls.Add(this.wrongLogin);
+            this.Controls.Add(this.forgotPass);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.bypass);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.emailTextBox);
+            this.Name = "LoginPage";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox emailTextBox;
+        private Label emailLabel;
+        private Label passwordLabel;
+        private TextBox passwordTextBox;
+        private Button bypass;
+        private Button loginButton;
+        private Button registerButton;
+        private Label forgotPass;
+        private Label wrongLogin;
+    }
+}
