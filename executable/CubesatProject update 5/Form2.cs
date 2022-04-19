9c934b0358c57af10b9b1dc370b04f2852e7e1b8
Reginald Winans <91345094+Rwinans15@users.noreply.github.com>
2022-04-18 22:55:00 -0700

    Add files via upload

---
+++ executable/CubesatProject update 5/Form2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CubesatProject
+{
+    public partial class mainPage : Form
+    {
+        private string email;
+
+
+        public mainPage()
+        {
+            InitializeComponent();
+        
+        }
+
+        public mainPage(string currentemail)
+        {
+            InitializeComponent();
+            this.email = currentemail;
+        }
+        public void changeEmail(string newemail)
+        {
+            this.email = newemail;
+        }
+        private void mainPage_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+       
+        private void button4_Click(object sender, EventArgs e) //Settings Popup
+        {
+            Form settingspage = new settingsPage(this, email);
+            settingspage.Show();
+            
+        }
+
+        private void mainUserControl1_Load(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void aboutUsButton_Click(object sender, EventArgs e)
+        {
+            //Shows about us page
+            aboutUsPage1.Show();
+            mainUserControl1.Hide();
+           
+        }
+
+
+
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            //Shows home page
+            aboutUsPage1.Hide();
+            mainUserControl1.Show();
+        }
+
+
+        private void aboutUsPage1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void profilePage1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ executable/UserControl2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace CubesatProject
+{
+    public partial class aboutUsPage : UserControl
+    {
+        public aboutUsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutUsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutUsBackground_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void descriptionLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clickPicture_Click(object sender, EventArgs e)
+        {
+            var uri = "https://github.com/aaronscout622/AMSAT-DATA-COLLECTION";
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                UseShellExecute = true,
+                FileName = uri
+            };
+            System.Diagnostics.Process.Start(psi);
+        
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+           
+        }
+    }
+}
