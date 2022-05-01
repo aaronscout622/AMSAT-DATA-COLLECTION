7a0740834498e8b9c28645792a2f893b605579f3
Trystan Vasquez <98122202+tophthebandit@users.noreply.github.com>
2022-05-01 14:13:55 -0700

    Add files via upload

---
+++ executable/CubesatProject 8/Form2.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace CubesatProject
+{
+    partial class mainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.homeButton = new System.Windows.Forms.Button();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.aboutUsButton = new System.Windows.Forms.Button();
+            this.mainUserControl1 = new CubesatProject.mainUserControl();
+            this.aboutUsPage1 = new CubesatProject.aboutUsPage();
+            this.SuspendLayout();
+            // 
+            // homeButton
+            // 
+            this.homeButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.homeButton.Location = new System.Drawing.Point(1772, 30);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(130, 152);
+            this.homeButton.TabIndex = 12;
+            this.homeButton.Text = "HOME";
+            this.homeButton.UseVisualStyleBackColor = false;
+            this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.settingsButton.Location = new System.Drawing.Point(1957, 30);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(130, 152);
+            this.settingsButton.TabIndex = 14;
+            this.settingsButton.Text = "SETTINGS";
+            this.settingsButton.UseVisualStyleBackColor = false;
+            this.settingsButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // aboutUsButton
+            // 
+            this.aboutUsButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.aboutUsButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.aboutUsButton.Location = new System.Drawing.Point(2144, 30);
+            this.aboutUsButton.Name = "aboutUsButton";
+            this.aboutUsButton.Size = new System.Drawing.Size(130, 152);
+            this.aboutUsButton.TabIndex = 15;
+            this.aboutUsButton.Text = "ABOUT US";
+            this.aboutUsButton.UseVisualStyleBackColor = false;
+            this.aboutUsButton.Click += new System.EventHandler(this.aboutUsButton_Click);
+            // 
+            // mainUserControl1
+            // 
+            this.mainUserControl1.AutoScroll = true;
+            this.mainUserControl1.AutoSize = true;
+            this.mainUserControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.mainUserControl1.Location = new System.Drawing.Point(0, 0);
+            this.mainUserControl1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.mainUserControl1.Name = "mainUserControl1";
+            this.mainUserControl1.Size = new System.Drawing.Size(2325, 1386);
+            this.mainUserControl1.TabIndex = 16;
+            this.mainUserControl1.Load += new System.EventHandler(this.mainUserControl1_Load);
+            // 
+            // aboutUsPage1
+            // 
+            this.aboutUsPage1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.aboutUsPage1.Location = new System.Drawing.Point(0, 0);
+            this.aboutUsPage1.Name = "aboutUsPage1";
+            this.aboutUsPage1.Size = new System.Drawing.Size(2325, 1316);
+            this.aboutUsPage1.TabIndex = 17;
+            this.aboutUsPage1.Load += new System.EventHandler(this.aboutUsPage1_Load);
+            // 
+            // mainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2325, 1316);
+            this.Controls.Add(this.aboutUsButton);
+            this.Controls.Add(this.settingsButton);
+            this.Controls.Add(this.homeButton);
+            this.Controls.Add(this.mainUserControl1);
+            this.Controls.Add(this.aboutUsPage1);
+            this.Name = "mainPage";
+            this.Text = "CubeSat Project";
+            this.Load += new System.EventHandler(this.mainPage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button homeButton;
+        private Button settingsButton;
+        private Button aboutUsButton;
+        public mainUserControl mainUserControl1;
+        public aboutUsPage aboutUsPage1;
+    }
+}
---
+++ executable/CubesatProject 8/UserControl1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CubesatProject
+{
+    public partial class mainUserControl : UserControl
+    {
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+        MySqlDataAdapter adapter;
+        DataTable table = new DataTable();
+
+        public mainUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void controlPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ao85Check_CheckedChanged(object sender, EventArgs e)
+        {
+            adapter = new MySqlDataAdapter("SELECT * FROM A085max", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID "+"Field"+"Bits"+"Unit"+"Conversion"+"Module"+"Module_Num"+"Module_Line"+"Line_Type"+"short_name"+"details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM A085min", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID "+"Field"+"Bits"+"Unit"+"Conversion"+"Module"+"Module_Num"+"Module_Line"+"Line_Type"+"short_name"+"details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM A085rtt", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID "+"Field"+"Bits"+"Unit"+"Conversion"+"Module"+"Module_Num"+"Module_Line"+"Line_Type"+"short_name"+"details";;
+            listBoxData.ValueMember = "";
+            //Adds/Removes AO85 tab based on whether the box is adding a check or removing one
+            if (ao85Check.Checked)
+            {
+                dataDisplay.TabPages.Add(ao85Tab);
+                int tabCount = dataDisplay.TabCount;
+                TabPage ao85 = dataDisplay.TabPages[tabCount - 1];  //Uses number of tabs currently in the container to find index of ao85
+                ao85.Text = "AO85";
+
+            }
+            else
+            {
+                dataDisplay.TabPages.Remove(ao85Tab);
+            }
+        }
+
+
+
+        private void ao91Check_CheckedChanged(object sender, EventArgs e)
+        {
+            adapter = new MySqlDataAdapter("SELECT * FROM A091max", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM A091min", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM A091rtt", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details"; ;
+            listBoxData.ValueMember = "";
+            //Adds/Removes AO91 tab based on whether the box is adding a check or removing one
+            if (ao91Check.Checked)
+            {
+                dataDisplay.TabPages.Add(ao91Tab);
+                int tabCount = dataDisplay.TabCount;
+                TabPage ao91 = dataDisplay.TabPages[tabCount - 1]; //Uses number of tabs currently in the container to find index of ao91
+                ao91.Text = "AO91";
+
+            }
+            else
+            {
+                dataDisplay.TabPages.Remove(ao91Tab);
+            }
+        }
+
+        private void ao95Check_CheckedChanged(object sender, EventArgs e)
+        {
+            adapter = new MySqlDataAdapter("SELECT * FROM A095max", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM A095min", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM A095rtt", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details"; ;
+            listBoxData.ValueMember = "";
+            //Adds/Removes AO95 tab based on whether the box is adding a check or removing one
+            if (ao95Check.Checked)
+            {
+                dataDisplay.TabPages.Add(ao95Tab);
+                int tabCount = dataDisplay.TabCount;
+                TabPage ao95 = dataDisplay.TabPages[tabCount - 1]; //Uses number of tabs currently in the container to find index of ao95
+                ao95.Text = "AO95";
+
+            }
+            else
+            {
+                dataDisplay.TabPages.Remove(ao95Tab);
+            }
+        }
+
+        private void ao92Check_CheckedChanged(object sender, EventArgs e)
+        {
+            adapter = new MySqlDataAdapter("SELECT * FROM A092max", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM A092min", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM A092rtt", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details"; ;
+            listBoxData.ValueMember = "";
+            //Adds/Removes AO92 tab based on whether the box is adding a check or removing one
+            if (ao92Check.Checked)
+            {
+                dataDisplay.TabPages.Add(ao92Tab);
+                int tabCount = dataDisplay.TabCount;
+                TabPage ao92 = dataDisplay.TabPages[tabCount - 1]; //Uses number of tabs currently in the container to find index of ao92
+                ao92.Text = "AO92";
+
+            }
+            else
+            {
+                dataDisplay.TabPages.Remove(ao92Tab);
+            }
+        }
+
+        private void fox1eCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            adapter = new MySqlDataAdapter("SELECT * FROM fox1emax", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM fox1emin", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM fox1ertt", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details"; ;
+            listBoxData.ValueMember = "";
+            //Adds/Removes Fox1E tab based on whether the box is adding a check or removing one
+            if (fox1eCheck.Checked)
+            {
+                dataDisplay.TabPages.Add(fox1eTab);
+                int tabCount = dataDisplay.TabCount;
+                TabPage fox1e = dataDisplay.TabPages[tabCount - 1]; //Uses number of tabs currently in the container to find index of Fox1E
+                fox1e.Text = "Fox1E";
+
+            }
+            else
+            {
+                dataDisplay.TabPages.Remove(fox1eTab);
+            }
+        }
+
+        private void ho107Check_CheckedChanged(object sender, EventArgs e)
+        {
+            adapter = new MySqlDataAdapter("SELECT * FROM ho107max", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM ho107min", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM ho107rtt", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details"; ;
+            listBoxData.ValueMember = "";
+            //Adds/Removes HO107 tab based on whether the box is adding a check or removing one
+            if (ho107Check.Checked)
+            {
+                dataDisplay.TabPages.Add(ho107Tab);
+                int tabCount = dataDisplay.TabCount;
+                TabPage ho107 = dataDisplay.TabPages[tabCount - 1]; //Uses number of tabs currently in the container to find index of HO107
+                ho107.Text = "HO107";
+
+            }
+            else
+            {
+                dataDisplay.TabPages.Remove(ho107Tab);
+            }
+        }
+
+        private void golftCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            adapter = new MySqlDataAdapter("SELECT * FROM golftmax", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM golftmin", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details";
+            listBoxData.ValueMember = "";
+            adapter = new MySqlDataAdapter("SELECT * FROM golftrtt", connection);
+            adapter.Fill(table);
+            listBoxData.DataSource = table;
+            listBoxData.DisplayMember = "NumberID " + "Field" + "Bits" + "Unit" + "Conversion" + "Module" + "Module_Num" + "Module_Line" + "Line_Type" + "short_name" + "details"; ;
+            listBoxData.ValueMember = "";
+            //Adds/Removes GolfT tab based on whether the box is adding a check or removing one
+            if (golftCheck.Checked)
+            {
+                dataDisplay.TabPages.Add(golftTab);
+                int tabCount = dataDisplay.TabCount;
+                TabPage golft = dataDisplay.TabPages[tabCount - 1]; //Uses number of tabs currently in the container to find index of GolfT
+                golft.Text = "GolfT";
+
+            }
+            else
+            {
+                dataDisplay.TabPages.Remove(golftTab);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Radio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void chartControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chartControl1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
